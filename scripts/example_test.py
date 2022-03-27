6b43d52b50e77ce5b712b05574db2bdbed88053b
Ubuntu <ubuntu@ip-192-168-39-183.ec2.internal>
2022-03-27 06:36:44 +0000

    updating example_test.py

--- scripts/example_test.py
+++ scripts/example_test.py
@@ -1,13 +1,12 @@
-
-m selenium import webdriver
+from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
 driver = webdriver.Firefox()
-driver.get("https://www.google.com/")
+driver.get("https://www.google.com")
 assert "google" in driver.title
 elem = driver.find_element_by_name("q")
 elem.clear()
-elem.send_keys("amazon.com")
+elem.send_keys("amazon")
 elem.send_keys(Keys.RETURN)
 assert "No results found." not in driver.page_source
 driver.close()
